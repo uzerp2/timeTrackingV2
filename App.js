41e3f3fff0f2266822f2f83c69aa8ebb7f560201
PULS\p.semin <uzerp2apple@gmail.com>
2019-05-20 17:41:09 +0300

    // 5-ый урок

--- App.js
+++ App.js
@@ -306,10 +306,125 @@
 
 
 
+// ////////////////////////////////////////////////////////////////////////////////////////
+// // 4-ый урок
+
+
+
+// import React from 'react';
+// import uuidv4 from 'uuid/v4';
+
+// import { StyleSheet, View, ScrollView, Text } from 'react-native';
+
+// import { newTimer } from './src/time-tracking/utils/TimerUtils';
+// import EditableTimer from './src/time-tracking/4/components/EditableTimer';
+// import ToggleableTimerForm from './src/time-tracking/4/components/ToggleableTimerForm';
+
+// export default class App extends React.Component {
+//   state = {
+//     timers: [
+//       {
+//         title: 'Mow the lawn',
+//         project: 'House Chores',
+//         id: uuidv4(),
+//         elapsed: 5456099,
+//         isRunning: true,
+//       },
+//       {
+//         title: 'Bake squash',
+//         project: 'Kitchen Chores',
+//         id: uuidv4(),
+//         elapsed: 1273998,
+//         isRunning: false,
+//       },
+//     ],
+//   };
+
+//   handleCreateFormSubmit = timer => {
+//     const { timers } = this.state;
+
+//     this.setState({
+//       timers: [newTimer(timer), ...timers],
+//     });
+//   };
+
+//   handleFormSubmit = attrs => {
+//     const { timers } = this.state;
+
+//     this.setState({
+//       timers: timers.map(timer => {
+//         if (timer.id === attrs.id) {
+//           const { title, project } = attrs;
+
+//           return {
+//             ...timer,
+//             title,
+//             project,
+//           };
+//         }
+
+//         return timer;
+//       }),
+//     });
+//   };
+
+//   render() {
+//     const { timers } = this.state;
+
+//     return (
+//       <View style={styles.appContainer}>
+//         <View style={styles.titleContainer}>
+//           <Text style={styles.title}>Timers</Text>
+//         </View>
+//         <ScrollView style={styles.timerList}>
+//           <ToggleableTimerForm
+//             onFormSubmit={this.handleCreateFormSubmit}
+//           />
+//           {timers.map(
+//             ({ title, project, id, elapsed, isRunning }) => (
+//               <EditableTimer
+//                 key={id}
+//                 id={id}
+//                 title={title}
+//                 project={project}
+//                 elapsed={elapsed}
+//                 isRunning={isRunning}
+//                 onFormSubmit={this.handleFormSubmit}
+//               />
+//             ),
+//           )}
+//         </ScrollView>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   appContainer: {
+//     flex: 1,
+//   },
+//   titleContainer: {
+//     paddingTop: 35,
+//     paddingBottom: 15,
+//     borderBottomWidth: 1,
+//     borderBottomColor: '#D6D7DA',
+//   },
+//   title: {
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//     textAlign: 'center',
+//   },
+//   timerList: {
+//     paddingBottom: 15,
+//   },
+// });
+
+
+
+
+
 ////////////////////////////////////////////////////////////////////////////////////////
-// 4-ый урок
-
-
+// 5-ый урок
 
 import React from 'react';
 import uuidv4 from 'uuid/v4';
@@ -317,8 +432,8 @@ import uuidv4 from 'uuid/v4';
 import { StyleSheet, View, ScrollView, Text } from 'react-native';
 
 import { newTimer } from './src/time-tracking/utils/TimerUtils';
-import EditableTimer from './src/time-tracking/4/components/EditableTimer';
-import ToggleableTimerForm from './src/time-tracking/4/components/ToggleableTimerForm';
+import EditableTimer from './src/time-tracking/5/components/EditableTimer';
+import ToggleableTimerForm from './src/time-tracking/5/components/ToggleableTimerForm';
 
 export default class App extends React.Component {
   state = {
@@ -368,6 +483,12 @@ export default class App extends React.Component {
     });
   };
 
+  handleRemovePress = timerId => {
+    this.setState({
+      timers: this.state.timers.filter(t => t.id !== timerId),
+    });
+  };
+
   render() {
     const { timers } = this.state;
 
@@ -390,6 +511,7 @@ export default class App extends React.Component {
                 elapsed={elapsed}
                 isRunning={isRunning}
                 onFormSubmit={this.handleFormSubmit}
+                onRemovePress={this.handleRemovePress}
               />
             ),
           )}
